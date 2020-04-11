7499c12695315758c63cc628362d58fe9d77322d
AreejAljeraiwi <48260156+AreejAljeraiwi@users.noreply.github.com>
2020-04-11 06:15:19 +0300

    Create like.php

---
+++ like.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "raasmah";
+
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$db);
+$PostID = $_POST["dis"];
+
+
+
+$sql = "UPDATE artwork SET LikeNum= LikeNum+ 1 WHERE id=" .$PostID ;
+
+if (mysqli_query($conn, $sql)) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . mysqli_error($conn);
+}
+header('Location: http://localhost/comment/ViewArtWork.php');
+
+?>
