331ee5a2bf60586fd5ef1e0f82843eea985b0d8f
AreejAljeraiwi <48260156+AreejAljeraiwi@users.noreply.github.com>
2020-04-11 19:09:11 +0300

    Update ViewArtWork.php

--- ViewArtWork.php
+++ ViewArtWork.php
@@ -456,14 +456,14 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = "SELECT text FROM artwork  WHERE ID = '1' ";
+$sql = "SELECT Description FROM artwork  WHERE ID = '1' ";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
         
-        echo "<strong> " . $row["text"].  "</strong>";
+        echo "<strong> " . $row["Description"].  "</strong>";
     }
 } else {
     echo "";
