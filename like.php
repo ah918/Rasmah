f6e8d9083ed2b87ac927476111acc13ca97d025f
AreejAljeraiwi <48260156+AreejAljeraiwi@users.noreply.github.com>
2020-04-12 00:04:06 +0300

    Update like.php

--- like.php
+++ like.php
@@ -1,4 +1,39 @@
 <?php
+session_start();
+ob_start();
+
+include('Conn.php');
+
+if(($_SESSION['role'] !='' || $_SESSION['role']=='visitor')){
+ 
+
+}
+else
+{echo '<script> window.location="login.php";</script>';
+  die("Redirecting to login.php");
+exit(); }
+/*$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);*/
+
+//include 'Conn.php';
+
+
+//$_SESSION['email']=$email;
+if(!isset($_SESSION['email'])) {
+  //mysql_close($connection);  
+ header("location : login.php");
+ exit();}
+
+
+
+
+?>
+
+<?php
 $servername = "localhost";
 $username = "root";
 $password = "";
