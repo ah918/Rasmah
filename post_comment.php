d7e5cb8d08b9447c691331159347949fc8fb9c44
AreejAljeraiwi <48260156+AreejAljeraiwi@users.noreply.github.com>
2020-04-12 00:04:09 +0300

    Update post_comment.php

--- post_comment.php
+++ post_comment.php
@@ -1,4 +1,39 @@
 <?php
+session_start();
+ob_start();
+
+include('Conn.php');
+
+if(($_SESSION['role'] !='' || $_SESSION['role']=='visitor')){
+ 
+
+}
+else
+{echo '<script> window.location="login.php";</script>';
+  die("Redirecting to login.php");
+exit(); }
+/*$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);*/
+
+//include 'Conn.php';
+
+
+//$_SESSION['email']=$email;
+if(!isset($_SESSION['email'])) {
+  //mysql_close($connection);  
+ header("location : login.php");
+ exit();}
+
+
+
+
+?>
+
+<?php
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -8,8 +43,8 @@ $db = "raasmah";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password,$db);
-$user_id = 2;
-$post_id = 1; 
+$user_id = $_SESSION['email'];
+$post_id = $_POST["Areej"];
 
 /*if (isset($_POST['action'])) {
     $post_id = $_POST['post_id'];
